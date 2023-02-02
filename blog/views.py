c43f9bbd23a3bc2d841946bbe52ece6bf50d6363
valentine-ochieng <ojvalentine14@gmail.com>
2023-02-02 23:32:05 +0300

    added styling

--- blog/views.py
+++ blog/views.py
@@ -12,10 +12,10 @@ def index(request):
         for file in files:
             new_file=Files(file=file)
             new_file.save()
-            # file_list.append(new_file.file.url)
+            file_list.append(new_file.file.url)
             
         # return render(request, 'index.html', {'new_urls': str(new_file.file.url)})
-        return render(request, 'index.html')
+        return render(request, 'index.html', {'new_urls': file_list})
     else:
         return render (request, 'index.html')
 
